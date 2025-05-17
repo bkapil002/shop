e6858a102a63bb55d980cece5919200f9fc6ccff
bkapil002 <kapilabrahm@gmail.com>
2025-05-17 11:50:35 +0530

    chnage

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.CLIENT_URL 
-    : ['http://localhost:3000','http://localhost:5174'],
+    : ['http://localhost:3000','http://localhost:5173'],
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
